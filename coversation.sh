68982027781634a34c93c52e91ce8cae75350023
venkey27 <venkey271200@gmail.com>
2026-05-24 21:40:15 -0400

    conversaction b/w venkey and sudheer

--- coversation.sh
+++ coversation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-PERSON1="RANGA"
-PERSON2="SAM"
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1: lets drink beers"
 echo "$PERSON2: cheers!"
 echo "$PERSON1: how are you doing?"
 echo "$PERSON2: i am doing good, how about you?"
+echo "$PERSON1: i am doing good too, thanks for asking" 
