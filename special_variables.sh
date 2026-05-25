21e7beccdc36d4bab8abc413467adfd462f58499
venkey27 <venkey271200@gmail.com>
2026-05-25 15:23:18 -0400

    special variables

--- special_variables.sh
+++ special_variables.sh
@@ -3,3 +3,6 @@
 
 echo "all the variables passed to the script is $@"
 echo "who many variables passed to the script is $#"
+echo "first variable is $1"
+echo "second variable is $2"
+echo "script name $0"
