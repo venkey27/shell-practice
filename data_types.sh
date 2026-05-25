3cba259e723717bcdf5698e964045862cd388b26
venkey27 <venkey271200@gmail.com>
2026-05-25 17:36:48 -0400

    data types

---
+++ data_types.sh
@@ -0,0 +1,6 @@
+movies = ("avatar""bahubali" "avengers" "spiderman")
+echo "all the movies are ${movies[@]}"
+echo "first movie names is ${movies[0]}"
+echo "second movie name is ${movies[2]}"
+echo "thried movie name is ${movies[3]}"
+echo "fourth movie name is ${movies[4]}"
