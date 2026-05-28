1f510e0a074162cbb1526d42542bc55a0db2bd0d
venkey27 <venkey271200@gmail.com>
2026-05-28 13:42:27 -0400

    special variables

--- set.sh
+++ set.sh
@@ -6,7 +6,7 @@ LOGS_DIR=/var/log/shell-script
 LOGS_FILE=$LOGS_DIR/$0.log
 TIMESTAMP=$(date "+%Y-%m-%d  %H:%M:%S")
 trap 'echo "ERROR at line $LINENO": command: $BASH_COMMAND' ERR
-
+                                                                        
 if [ $USERID -ne 0 ]; then
     echo " run the script with ROOT USER "
     exit 1
