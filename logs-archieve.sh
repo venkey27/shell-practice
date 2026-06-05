757e72e44da369ad9491e6437bbdafe3bea4213c
venkey27 <venkey271200@gmail.com>
2026-06-05 19:15:21 -0400

    special variables

--- logs-archieve.sh
+++ logs-archieve.sh
@@ -37,11 +37,11 @@ ARCHIVE_FILE="$DEST_DIR/logs-archive-$TIMESTAMP.tar.gz"
 tar -czvf "$ARCHIVE_FILE" $FILES &>/dev/null # -C is used to change the directory to source dir before creating the archive, so that it does not include the full path of the file in the archive
 
 if [ $? -eq 0 ]; then
-    echo "Aerchive created success, delecting thefiles"
+    echo "Archive created successfully, deleting the files"
     while IFS= read -r FILE
     do
         rm -f "$FILE"
-        echo " Delect file: $FILE"
+        echo "Deleted file: $FILE"
     done <<< "$FILES"
 else
     echo "Failed to create archive"
