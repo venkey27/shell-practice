012eb756771b2f073c908c53549dad7f3ec85649
venkey27 <venkey271200@gmail.com>
2026-06-05 19:17:57 -0400

    special variables

--- disk-usage.sh
+++ disk-usage.sh
@@ -4,10 +4,5 @@ DISK_USAGE=$(df -hT | grep -v Filesystem)
 
 while IFS= read -r LINE
 do
-    FILESYSTEM=$(echo "$LINE" | awk '{print $1}')
-    TYPE=$(echo "$LINE" | awk '{print $2}')
-    SIZE=$(echo "$LINE" | awk '{print $3}')
-    USED=$(echo "$LINE" | awk '{print $4}')
-    AVAILABLE=$(echo "$LINE" | awk '{print $5}')
-    PERCENTAGE=$(echo "$LINE" | awk '{print $6}')
+  echo "$LINE"
 done <<< "$DISK_USAGE"
