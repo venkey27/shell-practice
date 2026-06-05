5cfc7e2a44efaf338114db97c700ad9496635e20
venkey27 <venkey271200@gmail.com>
2026-06-05 19:40:31 -0400

    special variables

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-
-while IFS= read -r LINE
+USAGE_THRESHOLD=70
+while IFS= read -r line # IFS is used to set the Internal Field Separator to newline character, so that it can handle file names with spaces
 do
-  echo "$LINE"
+  USAGE=$(echo "$line" | awk '{print $6}' | cut -d'%' -f1)
+  PARTITION=$(echo "$line" | awk '{print $7}')
+  if [ $USAGE -ge $USAGE_THRESHOLD ]; then
+    MESSAGE="High disk usage alert: Partition $PARTITION is at ${USAGE}% usage."
+  fi
 done <<< "$DISK_USAGE"
+
+echo"$MESSAGE"
